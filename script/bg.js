df05001e16c02a8fa58390c57a42e963b6aae10b
NaimurGit <naimur.turjo03@gmail.com>
2025-03-02 13:28:03 +0600

    After Final reveiw

--- script/bg.js
+++ script/bg.js
@@ -1,4 +1,4 @@
-const colors = [ "#2C3E50", "#3357FF", "#E74C3C", "#33FF57", "#9B59B6", "#C6E4D6", "#2A4D97", "#F39C12", "#1ABC9C", "#80B4FB", "#FF5733"];
+const colors = ["#FFFFFF", "#2C3E50", "#3357FF", "#E74C3C", "#33FF57", "#9B59B6", "#C6E4D6", "#2A4D97", "#F39C12", "#1ABC9C", "#80B4FB", "#FF5733"];
 let currentIndex = 0;
 
 function changeBackgroundColor() {
